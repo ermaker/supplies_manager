befddd9f50acad22aedba9b11d7594557c26010b
Minwoo Lee <ermaker@gmail.com>
2012-03-18 23:29:35 +0900

    add initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuppliesManager::Application.config.secret_token = '46ce3309d8fdc68953a8ff97377392eb6a58d019dbb7a01963fa7a774f076a7575d17e3a3e9700a300e00b28569e640c282fda11618518b47fd6b70c3601def5'
